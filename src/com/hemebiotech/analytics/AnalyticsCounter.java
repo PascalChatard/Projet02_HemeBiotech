ef93dcb13b91d602f1423d696903d82f4a5fe0b5
Pascal Chatard <pchatard@aol.com>
2021-05-17 12:26:58 +0200

    Correction des bugs et nettoyage code Alex.

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,39 +5,47 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int dialatedPupilCount = 0;
 	
+	/**
+	 * Trend analysis program, read asymptom's file, count the occurrences of three
+	 * symptom's type and write the result in a text file. This is the Alex's
+	 * version code correction.
+	 * 
+	 * @param args not used
+	 * @throws Exception untreated
+	 */
 	public static void main(String args[]) throws Exception {
-		// first get input
+		// open data source file
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			else if (line.equals("dialated pupils")) {
+				dialatedPupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine(); // get next symptom
 		}
+
+		// close input file text
+		reader.close();
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		// generate output result file
+		FileWriter writer = new FileWriter("results.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + dialatedPupilCount + "\n");
 		writer.close();
 	}
 }
